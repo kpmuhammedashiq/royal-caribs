4f8f06c0ea74bf8a1137b1d42b636d3f1d2a1003
kpmuhammedashiq <kpmuhammedashiq@gmail.com>
2025-05-24 23:33:57 +0530

    modules added

---
+++ offshore-server/src/main/java/com/royalcaribs/OffshoreServer.java
@@ -0,0 +1,12 @@
+package com.royalcaribs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffshoreServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffshoreServer.class,args);
+    }
+}
---
+++ ship/src/main/java/com/royalcaribs/ShipApplication.java
@@ -0,0 +1,11 @@
+package com.royalcaribs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShipApplication.class, args);
+    }
+}
